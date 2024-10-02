a653645bb77f0cd3dbf91bb82eba95d1a7c72b8d
namotswjhb024 <namotswjhb024@student.wethinkcode.co.za>
2024-10-02 15:42:43 +0200

    Priting the ADOP successfully

--- Student6.py/display_data.py
+++ Student6.py/display_data.py
@@ -1,8 +1,32 @@
 # display_data.py
 # Key learning points for the 6th student:
-
 # Displaying parsed data in a clean and readable way.
 # Understanding how to format and present API data.
+import json
+import requests
+
+#Extracts specific fields from the data 
+def parse_apod_info(data):
+    parse_info ={
+                "title":data.get("title"),
+                "explanation":data.get("explanation"),
+                "media_type":data.get("media_type"),
+                "url":data.get("url")
+    }
+    return parse_info
+
+#Retrieve the picture of the day data from NASA API
+def fetch_apod_info(api_key):
+    url ="https://api.nasa.gov/planetary/apod"
+    params = {'api_key': api_key}
+
+    response = requests.get(url, params=params)
+    if response.status_code == 200:
+        return parse_apod_info(response.json())
+    else:
+        print("Error getching data: {response.status_code}")
+        return None
+    
 
 def display_apod_info(info):
     """
@@ -16,3 +40,9 @@ def display_apod_info(info):
         print(f"URL: {info['url']}")
     else:
         print("No information to display.")
+
+#Fetch and displays the data when executed
+if __name__ == "__main__":
+    api_key = "DEMO_KEY"
+    apod_info = fetch_apod_info(api_key)
+    display_apod_info(apod_info)
