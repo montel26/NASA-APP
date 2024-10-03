9174add2917b8c44dac145bac2c20073292f7fbe
Sthembiso-Mfusi <stmfusijhb024@student.wethinkcode.co.za>
2024-10-03 09:21:36 +0200

    finished accounting for different errors but I left it open for the api to be changed

--- Student4.py/handle_errors.py
+++ Student4.py/handle_errors.py
@@ -3,13 +3,30 @@
 
 # Error handling with APIs (e.g., handling HTTP status codes).
 # Raising and catching exceptions.
+import requests
+response = requests.get('https://api.nasa.gov/planetary/apod?api_key=LIpDwAx5jRWNQxpeBWuiD40EPCyfRPuYXtSfIhLz')
+print(response.status_code)
 def handle_errors(response):
     """
     TODO: The 5th student will handle HTTP errors and validate the API response.
     """
     try:
         response.raise_for_status()  # Raises an exception for bad HTTP codes
-        return response.json()  # Return parsed JSON data if successful
+        return response.json()
+      # Return parsed JSON data if successful
     except Exception as err:
         print(f"Error fetching data: {err}")
         return None
+    except requests.exceptions.HTTPError as errh:
+        print("An Http Error occurred:" + repr(errh))
+        return None
+    except requests.exceptions.ConnectionError as errc:
+        print("An Error Connecting to the API occurred:" + repr(errc))
+        return None
+    except requests.exceptions.Timeout as errt:
+        print("A Timeout Error occurred:" + repr(errt))
+        return None
+    except requests.exceptions.RequestException as err:
+        print("An Unknown Error occurred" + repr(err))
+        return None
+print(handle_errors(response))
