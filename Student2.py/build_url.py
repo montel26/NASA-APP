57ab08189735cfe132f1fffe621b26ebee173b95
Phethego <phmotidjhb024@student.wethinkcode.co.za>
2024-10-03 20:59:44 +0200

    Coded for value error

--- Student2.py/build_url.py
+++ Student2.py/build_url.py
@@ -7,6 +7,9 @@ def build_url(api_key):
     TODO: The 2nd student needs to create a function that builds the NASA API URL.
     Use the API key to dynamically build the URL for the APOD endpoint.
     """
+    if not api_key:
+        raise ValueError ("API key is required.")
+    
     base_url = "https://api.nasa.gov/planetary/apod"
     url = f"{base_url}?api_key={api_key}"
     return url
