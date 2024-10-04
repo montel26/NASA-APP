cbe13ffad7475fd6491406c07cbdb9ad608a08b2
unknown <tumodisjhb024@student.wethinkcode.co.za>
2024-10-04 12:57:45 +0200

    completed fetch data

--- Student3.py/fetch_data.py
+++ Student3.py/fetch_data.py
@@ -5,13 +5,17 @@ import requests
 
 # Handling API responses.
 def fetch_apod_data(url):
+    success_status_code = 200
     """
     TODO: The 3rd will implement this function to fetch data from the NASA API.
     Use the 'requests' library to make an HTTP GET request.
     """
     try:
         response = requests.get(url)
-        return response
-    except Exception as err:
+        if response.status_code == success_status_code:
+            return response.json()
+        else:
+            print(f"error {response.status_code}")
+    except requests.exceptions.HTTPError as err:
         print(f"An error occurred: {err}")
         return None
